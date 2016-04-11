ad3dc47f26e7fcd11be499630ceb8bd0de188b3a
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-04-11 18:14:22 +0800

    added user account feature specs

--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 feature 'User Accounts' do
-  
+  scenario "mindvalley staff can login" do
+    sign_out
+    subject {create(:staff)}
+
+    visit root_page
+    click_link 'Login'
+
+    expect(page).to have_content subject.email
+  end
 end
