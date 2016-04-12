0c9c9ede04583ec82d914bb5e1d8840e0c6f741b
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-04-12 13:04:56 +0800

    modified specs

--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 feature 'User Accounts' do
   scenario "mindvalley staff can login" do
     visit root_path
-    click_link 'Login'
 
-    expect(page).to have_content subject.email
+    expect(page).to have_link 'Login'
   end
 end
