41c10a4b3e306d842bdb3ab214debd00251a0201
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-04-12 13:24:55 +0800

    added factory girl methods module

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "user access" do
+    context "for a mindvalley staff" do
+      subject { FactoryGirl.build(:staff) }
+
+      it "is allowed" do
+        expect(subject).to be_valid
+      end
+    end
+
+    it "is not allowed if user is not a mindvalley staff" do
+      let(:nonstaff) { build(:nonstaff) }
+
+      expect(:nonstaff).to_not be_valid
+    end
+  end
 end
