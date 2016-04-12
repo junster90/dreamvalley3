3be2c662377e4c3cd18a2e7e77f25ed5cfee1282
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-04-12 12:55:34 +0800

    added users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password "password"
+    password_confirmation "password"
+    confirmed_at Date.today
+
+    factory :staff do
+      email { Faker::Name.first_name.downcase + "@mindvalley.com" }
+    end
+
+    factory :nonstaff do
+      email { Faker::Name.first_name.downcase + "@gmail.com" }
+    end
   end
 end
